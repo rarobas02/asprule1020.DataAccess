535be442a316348df70d2881247597383995fc51
rarobas02 <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-04 23:33:11 +0800

    Update.js

---
+++ Migrations/20260304152102_AddEstFiledClosureDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asprule1020.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEstFiledClosureDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EstFiledClosureDate",
+                table: "Registers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstFiledClosureDate",
+                table: "Registers");
+        }
+    }
+}
