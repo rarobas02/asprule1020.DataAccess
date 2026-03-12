6841251dbd10b965259db64b336ff96cc12b83a3
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-12 22:28:00 +0800

    50. Removed SMTP email sender

---
+++ Repository/UpdateRepository.cs
@@ -0,0 +1,29 @@
+﻿using asprule1020.DataAccess.Data;
+using asprule1020.DataAccess.Repository.IRepository;
+using asprule1020.Models;
+using asprule1020.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace asprule1020.DataAccess.Repository
+{
+    public class UpdateRepository : Repository<Register>, IUpdateRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UpdateRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void UpdateRegistration(Register obj)
+        {
+            var entity = _db.Registers.FirstOrDefault(u => u.Id == obj.Id);
+            if (entity == null)
+                return;
+
+            //entity. = evaluatorFullName;
+        }
+    }
+}
