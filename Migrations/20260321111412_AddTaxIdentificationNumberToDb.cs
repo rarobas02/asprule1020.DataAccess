d312df6a6b8b4b92d71d354a3929cd8a71c2ea67
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-03-21 22:20:35 +0800

    Tax Identification Number

---
+++ Migrations/20260321111412_AddTaxIdentificationNumberToDb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace asprule1020.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaxIdentificationNumberToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EstTaxIdentificationNumber",
+                table: "Registers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstTaxIdentificationNumber",
+                table: "Registers");
+        }
+    }
+}
